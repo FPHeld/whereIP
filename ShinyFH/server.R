523c6ff5ae4f5ae7b36cfb63ff1b797cfdea98c3
Fabian Held <fabian.held@sydney.edu.au>
2016-07-30 22:30:41 +1000

    Westas map working in new layout

--- ShinyFH/server.R
+++ ShinyFH/server.R
@@ -1,23 +1,27 @@
 library(shiny)
-
+load('../Robjects/ipgodcl')  # replace this with a call to the API
 
 library(dplyr)
 # Define server logic required to draw a histogram
 #sliderValues <- reactive({
-source(ExploreSA3.R)      
+source("../ExploreSA3.R")      
 #}) 
 shinyServer(function(input, output) {
    
   output$IPMap <- renderLeaflet({
-        ipgod_2003 <- ipgod_cl %>% filter(application_year %in% c(input$yearrange[1]:input$yearrange[2]))
+        ipgod_sel <- ipgod_cl %>% 
+                      filter( application_year >= min(input$yearrange), 
+                              application_year <= max(input$yearrange))
         if(input$separate){
-        leaflet(data=ipgod_2003) %>% 
+        leaflet(data=ipgod_sel) %>% 
           setView(lng = 145.209511, lat = -33, zoom = 3) %>% 
           addCircles(~lon, ~lat, radius=1, #layerId=~zipcode,
-                     stroke=FALSE, fillOpacity=0.4, fillColor='blue') %>% 
+                     stroke=FALSE, 
+                     fillOpacity=0.4, 
+                     fillColor='blue') %>% 
           addTiles()     
         }else{
-        leaflet(data=ipgod_2003[,c('lat', 'lon')]) %>% 
+        leaflet(data=ipgod_sel[,c('lat', 'lon')]) %>% 
               addTiles() %>% 
               setView(lng = 145.209511, lat = -33, zoom = 3) %>% 
               addMarkers(clusterOptions = markerClusterOptions()) #%>% 
--- ShinyFH/ui.R
+++ ShinyFH/ui.R
@@ -10,39 +10,20 @@
 library(shiny)
 library(htmlwidgets)
 library(leaflet)
+library("pairsD3")
 
-# Define UI for application that draws a histogram
-shinyUI(
-  fluidPage(
-  # Application title
-  titlePanel("Discover the Australia IP Landscape"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       #sliderInput("year",
-       #            "Year",
-       #            min=2003, max=2015, value=2003,sep=""),
-       sliderInput("yearrange",
-                   "Year range",
-                   min=2003, max=2015, value=c(2003, 2005),sep=""),
-       checkboxInput("separate", "Do not cluster", FALSE)
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-      tabsetPanel(
-        tabPanel(title="Distribution of IP", #value="IPMap",
-                sliderInput(inputId="yearrange",
-                            label="Year range",
-                            min=2003, max=2015, 
-                            value=c(2003, 2005),
-                            sep=""),
-                checkboxInput("separate", "Do not cluster", FALSE),
-                leafletOutput("IPMap")
-        )
-    )
-  )
-  )
-) ## close fluid page
-) ## close shinyUI
+shinyUI(navbarPage("Discover the Australia IP Landscape",
+           tabPanel("Map",
+                    leafletOutput("IPMap"),
+                    sliderInput(inputId="yearrange",
+                                label="Year range",
+                                min=2003, max=2015, 
+                                value=c(2005, 2010),
+                                sep=""),
+                    checkboxInput("separate", "Do not cluster", FALSE)),
+           
+           tabPanel("Time"),
+           tabPanel("Demographics")
+)
+)
+
