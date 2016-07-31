859644870d01f719f7a182cecac185bc7d3d46be
Fabian Held <fabian.held@sydney.edu.au>
2016-07-31 10:12:51 +1000

    Corrected big plot

--- ShinyFH/ExplorePlotly.R
+++ ShinyFH/ExplorePlotly.R
@@ -18,7 +18,7 @@ ggplotly(Pat_Time_State_Lines, tooltip=c("y", "text"))
 
                                                
 
-ggplot(latest_AS3_data_BusiDEmPerf, aes(x=Nr_Busi, y=Education, text=SA3_Name, size=Nr_PatentApplicants) ) + 
+ggplot(latest_AS3_data_BusiDEmPerf, aes(x=Nr_Busi, y=Share_Ed, text=SA3_Name, size=Nr_PatentApplicants) ) + 
   facet_grid(Education ~ Type) + 
   geom_point() -> big_plot_BusiDEmPerfDem                              
                                            
